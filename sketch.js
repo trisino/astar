665c56e270c7277eb9f674119e82d596838c4eb9
Stefano Picchiotti <stefano_picchiotti@goodyear.com>
2018-06-28 10:47:56 +0200

    Added diagonal movement

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function heuristics (a,b){
   return d;
 }
 
-var cols = 50;
-var rows = 50;
+var cols = 150;
+var rows = 150;
 var grid = new Array(cols);
 var openSet = [];
 var closedSet = [];
@@ -33,7 +33,7 @@ function Spot(i, j) {
   this.previous = undefined;
   this.wall = false ;
 
-  if (random(1) < 0.3) {
+  if (random(1) < 0.2) {
     this.wall = true;
   }
 
@@ -49,18 +49,34 @@ function Spot(i, j) {
   this.addNeighbors = function(grid) {
     var i = this.i;
     var j = this.j;
-    if (i<cols-1){
-      this.neighbors.push(grid[i+1][j]);
-    }
-    if (i>0) {
-      this.neighbors.push(grid[i-1][j]);
-    }
-    if (j<rows-1){
-      this.neighbors.push(grid[i][j+1]);
-    }
-    if (j>0){
-      this.neighbors.push(grid[i][j-1]);
-    }
+
+    // for (i = 0; i < grid.length; i++) {
+    //         for (j = 0; j < grid[i].length; j++) {
+                for (var x = Math.max(0, i - 1); x <= Math.min(i + 1, grid.length); x++) {
+                    for (var y = Math.max(0, j - 1); y <= Math.min(j + 1, grid[i].length); y++) {
+                        if (x >= 0 && y >= 0 && x < grid.length && y < grid[i].length) {
+                            if(x!=i || y!=j){
+                              this.neighbors.push(grid[x][y]);
+                            }
+                        }
+                    }
+                }
+                // System.out.println("\n");
+    //         }
+    // }
+
+    // if (i<cols-1){
+    //   this.neighbors.push(grid[i+1][j]);
+    // }
+    // if (i>0) {
+    //   this.neighbors.push(grid[i-1][j]);
+    // }
+    // if (j<rows-1){
+    //   this.neighbors.push(grid[i][j+1]);
+    // }
+    // if (j>0){
+    //   this.neighbors.push(grid[i][j-1]);
+    // }
   }
 
 
