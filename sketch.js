45bad69038e9aac2e1f0cf383bd64f3bcf6af18a
stefano <me@my.home>
2018-06-28 03:30:53 +0200

    Coded Spot() object to populate the grid

--- sketch.js
+++ sketch.js
@@ -2,6 +2,12 @@ var cols = 5;
 var rows = 5;
 var grid = new Array(cols);
 
+function Spot() {
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+}
+
 function setup() {
     createCanvas(400,400);
     console.log("A*");
@@ -10,6 +16,13 @@ function setup() {
     for (var i=0; i < cols; i++) {
       grid[i] = new Array(rows);
     }
+
+    for (var i=0; i < cols; i++) {
+      for (var j=0; j < rows; j++) {
+        grid[i][j] = new Spot();
+      }
+    }
+
     console.log(grid);
 }
 
